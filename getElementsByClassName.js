999fa7b2e65c89ff83932db09a8a821012ef6d5f
robnyman <robnyman@54b48e31-2c4e-0410-85be-7f69e80301e4>
2008-05-27 10:39:32 +0000

    Added a meta-programming approach for much better performance

--- getElementsByClassName.js
+++ getElementsByClassName.js
@@ -1,62 +1,79 @@
 /*
-	Developed by Robert Nyman
-	Code/licensing: http://code.google.com/p/domassistant/
+	Developed by Robert Nyman, http://www.robertnyman.com
+	Code/licensing: http://code.google.com/p/getelementsbyclassname/
 */	
-function getElementsByClassName(className, tag, elm){
-	tag = tag || "*";
-	elm = (this !== window)? this : elm || document;
-	var classes = className.split(" "),
-		classesToCheck = "",
-		returnElements = [],
-		elements,
-		current;
-	if (elm.getElementsByClassName) {
-		var nodeName = new RegExp("\\b" + ((tag !== "*")? tag : "[a-z]+") + "\\b", "i");
-		elements = elm.getElementsByClassName(className);
-		for(var i=0, il=elements.length; i<il; i+=1){
-			current = elements[i];
-			if(nodeName.test(current.nodeName)) {
-				returnElements.push(current);
+var getElementsByClassName = function (className, tag, elm){
+	if (document.getElementsByClassName) {
+		getElementsByClassName = function (className, tag, elm) {
+			tag = tag || "*";
+			elm = elm || document;
+			var elements = elm.getElementsByClassName(className),
+				nodeName = new RegExp("\\b" + ((tag !== "*")? tag : "[a-z]+") + "\\b", "i"),
+				returnElements = [],
+				current;
+			for(var i=0, il=elements.length; i<il; i+=1){
+				current = elements[i];
+				if(nodeName.test(current.nodeName)) {
+					returnElements.push(current);
+				}
 			}
-		}
+			return returnElements;
+		};
 	}
 	else if (document.evaluate) {
-		var xhtmlNamespace = "http://www.w3.org/1999/xhtml",
-			namespaceResolver = (document.documentElement.namespaceURI === xhtmlNamespace)? xhtmlNamespace : null,
-			node;
+		getElementsByClassName = function (className, tag, elm) {
+			tag = tag || "*";
+			elm = elm || document;
+			var classes = className.split(" "),
+				classesToCheck = "",
+				xhtmlNamespace = "http://www.w3.org/1999/xhtml",
+				namespaceResolver = (document.documentElement.namespaceURI === xhtmlNamespace)? xhtmlNamespace : null,
+				returnElements = [],
+				elements,
+				node;
 			for(var j=0, jl=classes.length; j<jl; j+=1){
 				classesToCheck += "[contains(concat(' ', @class, ' '), ' " + classes[j] + " ')]";
 			}
-		try	{
-			elements = document.evaluate(".//" + tag + classesToCheck, elm, namespaceResolver, 0, null);
-		}
-		catch (e) {
-			elements = document.evaluate(".//" + tag + classesToCheck, elm, null, 0, null);
-		}
-		while ((node = elements.iterateNext())) {
-			returnElements.push(node);
-		}
+			try	{
+				elements = document.evaluate(".//" + tag + classesToCheck, elm, namespaceResolver, 0, null);
+			}
+			catch (e) {
+				elements = document.evaluate(".//" + tag + classesToCheck, elm, null, 0, null);
+			}
+			while ((node = elements.iterateNext())) {
+				returnElements.push(node);
+			}
+			return returnElements;
+		};
 	}
 	else {
-		classesToCheck = [];
-		elements = (tag === "*" && elm.all)? elm.all : elm.getElementsByTagName(tag);
-		var match;
-		for(var k=0, kl=classes.length; k<kl; k+=1){
-			classesToCheck.push(new RegExp("(^|\\s)" + classes[k] + "(\\s|$)"));
-		}
-		for(var l=0, ll=elements.length; l<ll; l+=1){
-			current = elements[l];
-			match = false;
-			for(var m=0, ml=classesToCheck.length; m<ml; m+=1){
-				match = classesToCheck[m].test(current.className);
-				if (!match) {
-					break;
+		getElementsByClassName = function (className, tag, elm) {
+			tag = tag || "*";
+			elm = elm || document;
+			var classes = className.split(" "),
+				classesToCheck = [],
+				elements = (tag === "*" && elm.all)? elm.all : elm.getElementsByTagName(tag),
+				current,
+				returnElements = [],
+				match;
+			for(var k=0, kl=classes.length; k<kl; k+=1){
+				classesToCheck.push(new RegExp("(^|\\s)" + classes[k] + "(\\s|$)"));
+			}
+			for(var l=0, ll=elements.length; l<ll; l+=1){
+				current = elements[l];
+				match = false;
+				for(var m=0, ml=classesToCheck.length; m<ml; m+=1){
+					match = classesToCheck[m].test(current.className);
+					if (!match) {
+						break;
+					}
+				}
+				if (match) {
+					returnElements.push(current);
 				}
 			}
-			if (match) {
-				returnElements.push(current);
-			}
-		}
-	}	
-	return returnElements;
-}
+			return returnElements;
+		};
+	}
+	return getElementsByClassName(className, tag, elm);
+};
